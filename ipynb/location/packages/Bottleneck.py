e796e8bca6acb5c2b27ec025b4cec8f79b3c5fc6
Bancie <144613141+Bancie@users.noreply.github.com>
2025-04-02 23:48:54 +0700

    Update Bottleneck.py

--- ipynb/location/packages/Bottleneck.py
+++ ipynb/location/packages/Bottleneck.py
@@ -1,1 +1,15 @@
 from sympy import symbols, Eq, solve
+
+class BottleNeck():
+    
+    def __init__(self, vertex_i, vertex_j, vertex):
+        self.vertex_i = vertex_i
+        self.vertex_j = vertex_j
+        self.vertex = vertex
+    
+    # def alphaOptimum ():
+        
+        
+
+
+# bn = BottleNeck(3,4)
