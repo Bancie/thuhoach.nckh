22af211504c4ed160136a7ae5c37fe197b9aa5a2
Bancie <144613141+Bancie@users.noreply.github.com>
2025-04-03 20:47:45 +0700

    up

--- ipynb/location/packages/Bottleneck.py
+++ ipynb/location/packages/Bottleneck.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from packages import ShortestPath
 
-class BottleNeck():
+class BP():
     
     def __init__(self, sourceGraph, vertices, vertex_i, vertex_j, vertex):
         self.sourceGraph = sourceGraph
@@ -23,21 +23,3 @@ class BottleNeck():
         x = symbols('x')
         equation = Eq(x * self.distance(self.vertex_i, self.vertex_j) + self.distance(self.vertex_i, self.vertex), (1-x)*self.distance(self.vertex_i, self.vertex_j) + self.distance(self.vertex_j, self.vertex))
         return solve(equation, x)
-
-# ----------------TEST CASE------------------
-
-graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-           [4, 0, 8, 0, 0, 0, 0, 11, 0],
-           [0, 8, 0, 7, 0, 4, 0, 0, 2],
-           [0, 0, 7, 0, 9, 14, 0, 0, 0],
-           [0, 0, 0, 9, 0, 10, 0, 0, 0],
-           [0, 0, 4, 14, 10, 0, 2, 0, 0],
-           [0, 0, 0, 0, 0, 2, 0, 1, 6],
-           [8, 11, 0, 0, 0, 0, 1, 0, 7],
-           [0, 0, 2, 0, 0, 0, 6, 7, 0]
-           ]
-
-a = infoma(graph, 9, 'A', 'B', 'C')
-
-# print(a.distance('A', 'B'))
-print(a.alpha())
---
+++ ipynb/location/packages/GraphView.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Graph():
+    
+    def __init__(self, src):
+        self.src = np.array(src)
+        
+    def show(self):
+        G = nx.from_numpy_array(self.src)
+        mapping = {i: chr(65 + i) for i in G.nodes}
+        G = nx.relabel_nodes(G, mapping)
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=1500, font_weight='bold')
+        edge_labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        plt.show()
