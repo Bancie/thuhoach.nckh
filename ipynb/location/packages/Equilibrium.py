d08caede754f8cc8685c09ce011ec12dbbdc5108
Bancie <144613141+Bancie@users.noreply.github.com>
2025-04-04 10:42:43 +0700

    up

---
+++ ipynb/location/packages/Equilibrium.py
@@ -0,0 +1,6 @@
+from sympy import symbols, Eq, solve
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from packages import ShortestPath
+from packages import Bottleneck
---
+++ ipynb/location/packages/Point.py
@@ -0,0 +1,46 @@
+from sympy import symbols, Eq, solve
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from packages import ShortestPath
+
+class Local():
+    
+    def __init__(self, sourceGraph, vertices, vertex_i, vertex_j, vertexA, vertexB):
+        self.sourceGraph = sourceGraph
+        self.vertices = vertices
+        self.vertex_i = vertex_i
+        self.vertex_j = vertex_j
+        self.vertexA = vertexA
+        self.vertexB = vertexB
+        
+    def distance(self, src, dest):
+        a = ShortestPath.Graph(self.vertices)
+        a.graph = self.sourceGraph
+        path, distance = a.getShortestPath(ord(src) - 65, ord(dest) - 65)
+        return distance
+    
+    def abstractAplha(self, A, B):
+        x = symbols('x')
+        equation = Eq(x * self.distance(self.vertex_i, self.vertex_j) + self.distance(self.vertex_i, A), (1-x)*self.distance(self.vertex_i, self.vertex_j) + self.distance(self.vertex_j, B))
+        return solve(equation, x)
+        
+        
+class BP(Local):
+    
+    def __init__(self, sourceGraph, vertices, vertex_i, vertex_j, vertexA):
+        super().__init__(sourceGraph, vertices, vertex_i, vertex_j, vertexA)
+    
+    def runBP(self):
+        return super().abstractAplha(self.vertexA, self.vertexA)
+        
+
+class EQ(Local):
+    
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        return super().abstractAplha(self.vertexA, self.vertexB)
+    
+    
